4bf9c1daff9798ba900abf9360597f5efba84710
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2022-11-06 01:19:21 +0200

    task04

--- js/task-04.js
+++ js/task-04.js
@@ -3,3 +3,28 @@
 //Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 //Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 //Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const spanValue = document.querySelector('#value');
+const btnMinus = document.querySelector('[data-action="decrement"]');
+const btnPlus = document.querySelector('[data-action="increment"]');
+
+let counterValue = parseInt(spanValue.textContent);
+
+btnMinus.addEventListener('click', () =>{
+counterValue -= 1;
+spanValue.textContent = counterValue;
+console.log(-1);
+} );
+
+btnPlus.addEventListener('click', () => {
+counterValue += 1;
+spanValue.textContent = counterValue;
+console.log(+1);
+});
+
+
+
+
+
+console.log(spanValue, btnMinus, btnPlus);
+console.log(counterValue);
